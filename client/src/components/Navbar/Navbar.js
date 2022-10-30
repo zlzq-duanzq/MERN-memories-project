e75eaaf9f78b735b0cd283fdfa4945b61d914c79
Zhongqian Duan <78020075+zlzq-duanzq@users.noreply.github.com>
2022-10-30 16:33:21 -0400

    Update Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -9,11 +9,11 @@ import decode from 'jwt-decode';
 import * as actionType from '../../constants/actionTypes';
 
 const Navbar = () => {
+    const classes = useStyles();
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
     const dispatch = useDispatch();
     const location = useLocation();
     const history = useNavigate();
-    const classes = useStyles();
 
     const logout = () => {
         dispatch({ type: actionType.LOGOUT });
@@ -42,7 +42,7 @@ const Navbar = () => {
                 <div className={classes.profile}>
                     <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
                     <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
-                    <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+                    <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
                 </div>
                 ) : (
                     <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
