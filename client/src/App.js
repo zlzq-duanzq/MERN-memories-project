35c10ac27bf656b78d841d62e8e80b7c97059041
Zhongqian Duan <78020075+zlzq-duanzq@users.noreply.github.com>
2022-11-04 19:57:08 -0400

    Pagination

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Auth from './components/Auth/Auth';
+import PostDetails from './components/PostDetails/PostDetails';
 
-const App = () => (
-  <BrowserRouter>
-    <Container maxWidth="lg">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
-    </Container>
-  </BrowserRouter>
-);
+const App = () => {
+  const user = JSON.parse(localStorage.getItem('profile'));
+
+  return (
+      <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={() => <Navigate to="/posts" />} />
+          <Route path="/posts" element={<Home />} />
+          <Route path="/posts/search" element={<Home />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/auth" element={() => (!user ? <Auth /> : <Navigate to="/posts"/>)} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostDetails = () => {
+
+    console.log("Post details");
+    return (
+        <div>PostDetails</div>
+    );
+};
+
+export default PostDetails;
